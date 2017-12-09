6e3f8b49caf92e43e8d2dc2376280a555ab6dc3c
Joshua Wootonn <joshuawootonn@gmail.com>
2017-12-08 21:59:52 -0600

    Some basic server code written. I need to learn more before I continue in that section.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,32 @@
 var express = require("express");
 var app = express();
-var port = 5656;
-app.get("/", (req,res) => {
-  res.send("Hello World");
+var port = 9898;
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/jello-server");
+
+var nameSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String
+});
+
+var User = mongoose.model("User", nameSchema);
+app.post("/addname", (req,res) =>{
+  var myData = new User(req.body);
+  myData.save()
+  .then(item =>{
+    res.send("item saved to database");
+  })
+  .catch(err => {
+    res.status(400).send("unable to save to database");
+  });
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/", (req,res) => {
+  res.sendfile(__dirname + "/index.html");
 });
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
